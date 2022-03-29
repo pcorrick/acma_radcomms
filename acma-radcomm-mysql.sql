2df8b7b15ea97ef264cd36a06a9bfe2a49999402
Phil Corrick <philip.corrick@gmail.com>
2022-03-29 04:50:08 +0000

    add foreign keys and ERD

--- acma-radcomm-mysql.sql
+++ acma-radcomm-mysql.sql
@@ -4,7 +4,6 @@ CREATE TABLE `access_area` (
   `AREA_NAME` varchar(256) COLLATE utf8mb4_unicode_ci NULL,
   `AREA_CATEGORY` tinyint NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `access_area` ADD PRIMARY KEY (`AREA_ID`);
 
 create table `antenna`(
  `ANTENNA_ID` varchar(31) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -21,7 +20,6 @@ create table `antenna`(
  `MODEL` varchar(80) COLLATE utf8mb4_unicode_ci NULL,
  `MANUFACTURER` varchar(255) COLLATE utf8mb4_unicode_ci NULL
  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `antenna` ADD PRIMARY KEY (`ANTENNA_ID`);
 
 create table antenna_pattern(
  `ANTENNA_ID` varchar(31) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -35,7 +33,6 @@ create table antenna_polarity(
  `POLARISATION_CODE` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
  `POLARISATION_TEXT` varchar(50) COLLATE utf8mb4_unicode_ci NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `antenna_polarity` ADD PRIMARY KEY (`POLARISATION_CODE`);
 
 create table applic_text_block(
  `APTB_ID`		int NOT NULL,
@@ -84,15 +81,10 @@ CREATE TABLE `bsl_area` (
   `AREA_NAME` varchar(256) COLLATE utf8mb4_unicode_ci NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `bsl_area` ADD PRIMARY KEY (`AREA_CODE`);
-
 CREATE TABLE `class_of_station` (
-  `CODE` varchar(31) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CODE` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
   `DESCRIPTION` varchar(511) COLLATE utf8mb4_unicode_ci NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-ALTER TABLE `class_of_station` ADD PRIMARY KEY (`CODE`);
-
 
 CREATE TABLE `client` (
   `CLIENT_NO` int NOT NULL,
@@ -104,21 +96,15 @@ CREATE TABLE `client` (
   `POSTAL_SUBURB` varchar(480) COLLATE utf8mb4_unicode_ci NULL,
   `POSTAL_STATE` varchar(36) COLLATE utf8mb4_unicode_ci NULL,
   `POSTAL_POSTCODE` varchar(72) COLLATE utf8mb4_unicode_ci NULL,
-  `CAT_ID` decimal(30,2) NULL,
-  `CLIENT_TYPE_ID` decimal(30,2) NULL,
-  `FEE_STATUS_ID` decimal(30,2) NULL
+  `CAT_ID` int NULL,
+  `CLIENT_TYPE_ID` int NULL,
+  `FEE_STATUS_ID` int NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-ALTER TABLE `client` ADD PRIMARY KEY (`CLIENT_NO`);
-
 
 CREATE TABLE `client_type` (
   `TYPE_ID` int NOT NULL,
   `NAME` varchar(240) COLLATE utf8mb4_unicode_ci NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-ALTER TABLE `client_type` ADD PRIMARY KEY (`TYPE_ID`);
-
 
 create table device_details(
  `SDD_ID` int NOT NULL,
@@ -146,24 +132,24 @@ create table device_details(
  `LEVEL_OF_PROTECTION` decimal(30,2) NULL,
  `EIRP` decimal(30,2) NULL,
  `EIRP_UNIT` varchar(31) COLLATE utf8mb4_unicode_ci NULL,
- `SV_ID` decimal(30,2) NULL,
- `SS_ID` decimal(30,2) NULL,
+ `SV_ID` int NULL,
+ `SS_ID` int NULL,
  `EFL_ID` varchar(40) COLLATE utf8mb4_unicode_ci NULL,
  `EFL_FREQ_IDENT` varchar(31) COLLATE utf8mb4_unicode_ci NULL,
  `EFL_SYSTEM` varchar(63) COLLATE utf8mb4_unicode_ci NULL,
  `LEQD_MODE` varchar(1) COLLATE utf8mb4_unicode_ci NULL,
  `RECEIVER_THRESHOLD` decimal(30,2) NULL,
- `AREA_AREA_ID` decimal(30,2) NULL,
+ `AREA_AREA_ID` int NULL,
  `CALL_SIGN` varchar(255) COLLATE utf8mb4_unicode_ci NULL,
  `AREA_DESCRIPTION` varchar(9) COLLATE utf8mb4_unicode_ci NULL,
  `AP_ID` decimal(30,2) NULL,
- `CLASS_OF_STATION_CODE` varchar(31) COLLATE utf8mb4_unicode_ci NULL,
+ `CLASS_OF_STATION_CODE` varchar(6) COLLATE utf8mb4_unicode_ci NULL,
  `SUPPLIMENTAL_FLAG` varchar(199) COLLATE utf8mb4_unicode_ci NULL,
  `EQ_FREQ_RANGE_MIN` decimal(30,2) NULL,
  `EQ_FREQ_RANGE_MAX` decimal(30,2) NULL,
- `NATURE_OF_SERVICE_ID` varchar(3) COLLATE utf8mb4_unicode_ci NULL,
+ `NATURE_OF_SERVICE_ID` varchar(6) COLLATE utf8mb4_unicode_ci NULL,
  `HOURS_OF_OPERATION` varchar(11) COLLATE utf8mb4_unicode_ci NULL,
- `SA_ID` decimal(30,2) NULL,
+ `SA_ID` int NULL,
  `RELATED_EFL_ID` decimal(30,2) NULL,
  `EQP_ID` decimal(30,2) NULL,
  `ANTENNA_MULTI_MODE` varchar(3) COLLATE utf8mb4_unicode_ci NULL,
@@ -176,7 +162,6 @@ create table device_details(
  `STATION_TYPE` varchar(511) COLLATE utf8mb4_unicode_ci NULL,
  `STATION_NAME` varchar(63) COLLATE utf8mb4_unicode_ci NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `device_details` ADD PRIMARY KEY (`DEVICE_REGISTRATION_IDENTIFIER`);
 
 create table fee_status(
  `FEE_STATUS_ID` int NOT NULL,
@@ -191,9 +176,9 @@ create table industry_cat(
 
 create table licence(
  `LICENCE_NO` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL,
- `CLIENT_NO` decimal(30,2) NULL,
- `SV_ID` decimal(30,2) NULL,
- `SS_ID` decimal(30,2) NULL,
+ `CLIENT_NO` int NULL,
+ `SV_ID` int NULL,
+ `SS_ID` int NULL,
  `LICENCE_TYPE_NAME` varchar(63) COLLATE utf8mb4_unicode_ci NULL,
  `LICENCE_CATEGORY_NAME` varchar(95) COLLATE utf8mb4_unicode_ci NULL,
  `DATE_ISSUED` date,
@@ -219,18 +204,17 @@ create table licence_status(
 
 create table licence_subservice(
  `SS_ID` int NOT NULL,
- `SV_SV_ID` decimal(30,2) NULL,
+ `SV_SV_ID` int NULL,
  `SS_NAME` varchar(95) COLLATE utf8mb4_unicode_ci NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 create table licensing_area(
- `LICENSING_AREA_ID` tinyint COLLATE utf8mb4_unicode_ci NOT NULL,
+ `LICENSING_AREA_ID` tinyint NOT NULL,
  `DESCRIPTION` varchar(511) COLLATE utf8mb4_unicode_ci NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `licensing_area` ADD PRIMARY KEY (`LICENSING_AREA_ID`);
 
 create table nature_of_service(
- `CODE` varchar(31) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `CODE` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
  `DESCRIPTION` varchar(511) COLLATE utf8mb4_unicode_ci NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -267,8 +251,61 @@ create table site(
 
 
 /**
+ * Create primary keys.
+ */
+
+ALTER TABLE `access_area` ADD PRIMARY KEY (`AREA_ID`);
+ALTER TABLE `antenna` ADD PRIMARY KEY (`ANTENNA_ID`);
+ALTER TABLE `antenna_polarity` ADD PRIMARY KEY (`POLARISATION_CODE`);
+ALTER TABLE `auth_spectrum_area` ADD PRIMARY KEY (`LICENCE_NO`);
+ALTER TABLE `auth_spectrum_freq` ADD PRIMARY KEY (`LICENCE_NO`);
+ALTER TABLE `applic_text_block` ADD PRIMARY KEY (`APTB_ID`);
+ALTER TABLE `bsl` ADD PRIMARY KEY (`BSL_NO`);
+ALTER TABLE `bsl_area` ADD PRIMARY KEY (`AREA_CODE`);
+ALTER TABLE `class_of_station` ADD PRIMARY KEY (`CODE`);
+ALTER TABLE `client` ADD PRIMARY KEY (`CLIENT_NO`);
+ALTER TABLE `client_type` ADD PRIMARY KEY (`TYPE_ID`);
+ALTER TABLE `device_details` ADD PRIMARY KEY (`DEVICE_REGISTRATION_IDENTIFIER`);
+ALTER TABLE `fee_status` ADD PRIMARY KEY (`FEE_STATUS_ID`);
+ALTER TABLE `industry_cat` ADD PRIMARY KEY (`CAT_ID`);
+ALTER TABLE `licensing_area` ADD PRIMARY KEY (`LICENSING_AREA_ID`);
+ALTER TABLE `licence` ADD PRIMARY KEY (`LICENCE_NO`);
+ALTER TABLE `licence_subservice` ADD PRIMARY KEY (`SS_ID`);
+ALTER TABLE `licence_service` ADD PRIMARY KEY (`SV_ID`);
+ALTER TABLE `licence_status` ADD PRIMARY KEY (`STATUS`);
+ALTER TABLE `nature_of_service` ADD PRIMARY KEY (`CODE`);
+ALTER TABLE `reports_text_block` ADD PRIMARY KEY (`RTB_ITEM`);
+ALTER TABLE `site` ADD PRIMARY KEY (`SITE_ID`);
+ALTER TABLE `satellite` ADD PRIMARY KEY (`SA_ID`);
+
+/**
  * Create foreign key constraints.
  */
 
 ALTER TABLE `access_area` ADD CONSTRAINT `AREA_CATEGORY_foreign` FOREIGN KEY(`AREA_CATEGORY`) REFERENCES `licensing_area`(`LICENSING_AREA_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `antenna_pattern` ADD CONSTRAINT `ANTENNA_ID_foreign` FOREIGN KEY(`ANTENNA_ID`) REFERENCES `antenna`(`ANTENNA_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `applic_text_block` ADD CONSTRAINT `ATB_LICENCE_NO_foreign` FOREIGN KEY(`LICENCE_NO`) REFERENCES `licence`(`LICENCE_NO`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `applic_text_block` ADD CONSTRAINT `ATB_APTB_ITEM_foreign` FOREIGN KEY(`APTB_ITEM`) REFERENCES `reports_text_block`(`RTB_ITEM`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `auth_spectrum_area` ADD CONSTRAINT `ASA_LICENCE_NO_foreign` FOREIGN KEY(`LICENCE_NO`) REFERENCES `licence`(`LICENCE_NO`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `auth_spectrum_freq` ADD CONSTRAINT `ASF_LICENCE_NO_foreign` FOREIGN KEY(`LICENCE_NO`) REFERENCES `licence`(`LICENCE_NO`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `bsl` ADD CONSTRAINT `AREA_CODE_foreign` FOREIGN KEY(`AREA_CODE`) REFERENCES `bsl_area`(`AREA_CODE`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `client` ADD CONSTRAINT `CAT_ID_foreign` FOREIGN KEY(`CAT_ID`) REFERENCES `industry_cat`(`CAT_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `client` ADD CONSTRAINT `CLIENT_TYPE_ID_foreign` FOREIGN KEY(`CLIENT_TYPE_ID`) REFERENCES `client_type`(`TYPE_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `client` ADD CONSTRAINT `FEE_STATUS_ID_foreign` FOREIGN KEY(`FEE_STATUS_ID`) REFERENCES `fee_status`(`FEE_STATUS_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_LICENCE_NO_foreign` FOREIGN KEY(`LICENCE_NO`) REFERENCES `licence`(`LICENCE_NO`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_SITE_ID_foreign` FOREIGN KEY(`SITE_ID`) REFERENCES `site`(`SITE_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_ANTENNA_ID_foreign` FOREIGN KEY(`ANTENNA_ID`) REFERENCES `antenna`(`ANTENNA_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_SS_ID_foreign` FOREIGN KEY(`SS_ID`) REFERENCES `licence_subservice`(`SS_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_SV_ID_foreign` FOREIGN KEY(`SV_ID`) REFERENCES `licence_service`(`SV_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_CLASS_OF_STATION_CODE_foreign` FOREIGN KEY(`CLASS_OF_STATION_CODE`) REFERENCES `class_of_station`(`CODE`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_AREA_ID_foreign` FOREIGN KEY(`AREA_AREA_ID`) REFERENCES `access_area`(`AREA_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_SA_ID_foreign` FOREIGN KEY(`SA_ID`) REFERENCES `satellite`(`SA_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_NATURE_OF_SERVICE_ID_foreign` FOREIGN KEY(`NATURE_OF_SERVICE_ID`) REFERENCES `nature_of_service`(`CODE`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `device_details` ADD CONSTRAINT `DD_POLARISATION_foreign` FOREIGN KEY(`POLARISATION`) REFERENCES `antenna_polarity`(`POLARISATION_CODE`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `licence` ADD CONSTRAINT `L_SS_ID_foreign` FOREIGN KEY(`SS_ID`) REFERENCES `licence_subservice`(`SS_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `licence` ADD CONSTRAINT `L_SV_ID_foreign` FOREIGN KEY(`SV_ID`) REFERENCES `licence_service`(`SV_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `licence` ADD CONSTRAINT `L_STATUS_foreign` FOREIGN KEY(`STATUS`) REFERENCES `licence_status`(`STATUS`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `licence` ADD CONSTRAINT `L_CLIENT_NO_foreign` FOREIGN KEY(`CLIENT_NO`) REFERENCES `client`(`CLIENT_NO`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `licence` ADD CONSTRAINT `L_BSL_NO_foreign` FOREIGN KEY(`BSL_NO`) REFERENCES `bsl`(`BSL_NO`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `licence_subservice` ADD CONSTRAINT `SV_SV_ID_foreign` FOREIGN KEY(`SV_SV_ID`) REFERENCES `licence_service`(`SV_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
 ALTER TABLE `site` ADD CONSTRAINT `LICENSING_AREA_ID_foreign` FOREIGN KEY(`LICENSING_AREA_ID`) REFERENCES `licensing_area`(`LICENSING_AREA_ID`) ON DELETE CASCADE ON UPDATE RESTRICT;
